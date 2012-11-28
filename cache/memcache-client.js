dbd0a0a808f0fe42a54705a26da812994347be70
Mohammad Khan <mohkhan@zynga.com>
2012-11-28 00:04:16 -0500

    Update MemcachedSessionStore validation for session auth

--- cache/memcache-client.js
+++ cache/memcache-client.js
@@ -3,8 +3,8 @@ util = require('util');
 
 // Module level constants
 var MEMCACHE_RETRY = 2; // Retire after trying to connect twice.
-var MEMCACHE_EXPIRATION = 86400; // One day in seconds.
-var MEMCACHE_SESSION_EXPIRATION = 86400;
+var MEMCACHE_EXPIRATION = 60 * 60 * 24 * 1000; // One day in milliseconds.
+var MEMCACHE_SESSION_EXPIRATION = 60 * 60 * 12 * 1000; // 0.5 day in milliseconds.
 /**
  * Creates a wrapper around the memjs library for functioning with memcache on
  * heroku, due to support for SASL authentication. memjs is able to read out the heroku-specifc
@@ -23,11 +23,15 @@ var MemcachedClient = module.exports.MemcachedClient = memjs.Client.create(proce
  * Wrapper around our existing MemcachedClient + connect.session.store.
  * Inherits form the connect Store to inherit functionality, and uses the MemcachedClient
  * for data serialization/deserialization from the cache
- * @param {[Object]} Store [Connect.session.store]
+ * @param {[Object]} Connect [Connect module containing SessionStore]
  */
-module.exports.MemcachedStore = function (Store) {
+module.exports.MemcachedStore = function (connect) {
 
-    function MemcachedStore() {
+    var Store = connect.session.Store;
+
+    function MemcachedStore(options) {
+        options = options || {};
+        Store.call(this, options);
         this.client = memjs.Client.create(process.env.MEMCACHE_SERVERS, {
             retires: 2,
             expires: MEMCACHE_SESSION_EXPIRATION,
@@ -43,29 +47,58 @@ module.exports.MemcachedStore = function (Store) {
     /**
      * Attempt to fetch session by the given 'sid'.
      * @param  {[String]}   sid    [session id]
-     * @param  {Function} callback [args: error, value (JSON.parse() required), extras]
+     * @param  {Function} callback [args: error, session]
      */
     MemcachedStore.prototype.get = function (sid, callback) {
-        this.client.get(sid, callback);
+        this.client.get(sid, function (err, value, flags) {
+            if (err) {
+                callback(err);
+            }
+
+            // Grab the session
+            var session = JSON.parse(value);
+
+            if (session.cookie.expires >= Date.now()) {
+                this.destroy(sid, function (err, success) {
+                    callback(new Error("Session has expired"));
+                });
+            } else {
+                callback(null, session);
+            }
+        });
     };
 
     /**
      * Commit the given 'sess' object associated with the given 'sid'
      * @param  {[String]}   sid    [session id]
-     * @param  {Function} callback [args: error, isSuccess, extras]
+     * @param  {Function} callback [args: error, success]
      */
     MemcachedStore.prototype.set = function (sid, session, callback) {
-        this.client.get(sid, JSON.stringify(session), callback);
+        this.client.destroy(sid, function (err, success) {});
+
+        if (!session.cookie.expires || session.cookie.expires >= Date.now()) {
+            session.cookie.expires = new Date(Date.now() + MEMCACHE_SESSION_EXPIRATION);
+        }
+        session.cookie.maxAge = session.cookie.maxAge || MEMCACHE_SESSION_EXPIRATION;
+
+        this.client.set(sid, JSON.stringify(session), function (err, success, flags) {
+            callback(err, success);
+        });
     };
 
     /**
      * Destroys the session associated with the given 'sid'.
      * @param  {[String]}   sid    [session id]
-     * @param  {Function} callback [args: error, isSuccess]
+     * @param  {Function} callback [args: error, success]
      */
     MemcachedStore.prototype.destroy = function (sid, callback) {
         this.client.delete(sid, callback);
     };
+
+
+    // TODO: Figure out how to bind the following methods
+    MemcachedStore.prototype.length = function (callback) {};
+    MemcachedStore.prototype.clear = function (callback) {};
 
     return MemcachedStore;
 };
