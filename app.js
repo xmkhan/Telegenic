228a80f9fa54b33cdcafe2b4f10a32d0344119d2
Mohammad Khan <mohkhan@zynga.com>
2012-12-01 15:43:11 -0500

    Add redis as cache + session store

--- app.js
+++ app.js
@@ -4,13 +4,15 @@
  */
 
 var express = require('express'),
+ url = require('url'),
  routes = require('./routes'),
  user = require('./routes/user'),
  user_auth = require('./routes/user_auth'),
  http = require('http'),
  path = require('path'),
  DB = require('./database'),
- MemcachedStore = require('./cache/memcache-client').MemcachedStore(express);
+ redis = require('redis'),
+ RedisStore = require('connect-redis')(express);
 
 
 DB.client.connect(function (err) {
@@ -52,6 +54,17 @@ DB.client.query(
 
 DB.client.end();
 
+// Heroku redis config
+var client;
+var REDIS_DATABASE_NAME = 'redistogo';
+if (process.env.REDISTOGO_URL) {
+    var redisURL =  url.parse(process.env.REDISTOGO_URL);
+    client = redis.createClient(redisURL.port, redisURL.host);
+    client.auth(redisURL.auth.split(':')[1]);
+} else {
+    client = redis.createClient();
+}
+
 var app = express();
 
 app.configure(function () {
@@ -62,10 +75,13 @@ app.configure(function () {
     app.use(express.logger('dev'));
     app.use(express.bodyParser());
     app.use(express.methodOverride());
-    app.use(express.cookieParser('a5563829ee69090e6828278fbd5d43f8'));
+    app.use(express.cookieParser(process.env.CLIENT_SECRET || 'a5563829ee69090e6828278fbd5d43f8'));
     app.use(express.session({
-        secret: 'a5563829ee69090e6828278fbd5d43f8',
-        store: new MemcachedStore()
+        secret: process.env.CLIENT_SECRET || 'a5563829ee69090e6828278fbd5d43f8',
+        maxAge: new Date(Date.now() + 86400000), // 24 hour lifetime
+        store: new RedisStore({
+            client: client
+        })
     }));
     app.use(app.router);
     app.use(express.static(path.join(__dirname, 'public/')));
