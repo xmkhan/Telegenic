caa0af9daa953146c283294b45d86ca87d6917ba
Mohammad Khan <mohkhan@zynga.com>
2012-12-18 02:44:31 -0500

    Add media mongodb store as instance method

--- models/media.js
+++ models/media.js
@@ -16,15 +16,14 @@ User = require('./user').User;
 var Media = sequelize.define('Media', {
   identifier: { type: Sequelize.STRING, allowNull: false, unique: true, validate: { isUUID: 4 }},
   name: { type: Sequelize.STRING, allowNull: false, validate: { isAlphanumeric: true }},
+  instanceMethods: { store: function (data) {
+      mongodb.collection('Media', function (er, collection) {
+        if (er) console.log(er); //TODO: add error handling?
+        collection.insert({ 'data': new Binary(data), 'identifier': this.identifier });
+      });
+    }}
 });
 
 Media.hasOne(User, {foreignKey: 'userId'});
-
-module.exports._store = function (Media, data) {
-  mongodb.collection('Media', function (er, collection) {
-    if (er) console.log(er); //TODO: add error handling?
-    collection.insert({ 'data': new Binary(data), 'identifier': Media.identifier });
-  });
-};
 
 module.exports.Media = Media;
